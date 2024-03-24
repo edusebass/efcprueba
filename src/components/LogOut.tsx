af1394b70990b6a59e16822d0f835706640c7bbc
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2024-03-23 21:52:11 -0500

    update

--- src/components/LogOut.tsx
+++ src/components/LogOut.tsx
@@ -1,17 +1,18 @@
 "use client"
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useState } from 'react';
 import { Breadcrumb, Button, Layout, Menu, theme } from 'antd';
 import { useAuth } from '@/context/AuthContext';
 
-
-
 const LogOut = () => {
-  
+  const [loading, setLoading] = useState(false)
   const { logout } = useAuth(); // Usa el hook useAuth para obtener la función de inicio de sesión del contexto de autenticación
 
+  // const loadingCharge = () => {
+  //   setLoading(true)
+  // }
   return (
     
-          <Button className='bg-blue-500' href='/' onClick={logout}> Salir </Button>
+    <Button className='bg-blue-500 text-white' href='/' onClick={logout} loading={loading}> Salir </Button>
         
   );
 };
--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -2,6 +2,7 @@
 import { useRouter } from 'next/navigation';
 import React, { useState } from 'react';
 import { useAuth } from '@/context/AuthContext';
+import { Button } from 'antd';
 
 export default function LoginForm() {
     const router = useRouter();
@@ -18,8 +19,10 @@ export default function LoginForm() {
 
         try {
             await login(email, password);
+            setLoading(true)
         } catch (error:any) {
             console.log(error)
+            setLoading(false)
         }
     };
 
@@ -41,9 +44,8 @@ export default function LoginForm() {
                                 <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
                             </div>
 
-                            <button type="submit" className={`w-full text-white bg-blue-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ${loading ? 'opacity-50 cursor-not-allowed' : ''}`} disabled={loading}>Ingresar</button>
+                            <Button type="primary" htmlType='submit' loading={loading} className={`w-full text-white bg-blue-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5  text-center ${loading ? 'opacity-50 cursor-not-allowed' : ''}`} disabled={loading}>Ingresar</Button>
 
-                            {error && <p className="text-red-500">{error}</p>}
                         </form>
                     </div>
                 </div>
